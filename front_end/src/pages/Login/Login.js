289389722dd7422867a609662a4533605c6cf1cf
quyhochanh148 <quytthe173122@fpt.edu.vn>
2025-06-18 22:54:11 +0700

    aaaka

--- front_end/src/pages/Login/Login.js
+++ front_end/src/pages/Login/Login.js
@@ -192,28 +192,11 @@ const LoginPage = () => {
                     <div className="mt-4 sm:mt-6">
                         <div className="relative">
                             <div className="absolute inset-0 flex items-center">
-                                {/* <div className="w-full border-t border-gray-300"></div> */}
+
                             </div>
-                            {/* <div className="relative flex justify-center text-xs sm:text-sm">
-                                <span className="px-4 bg-white text-gray-500">Hoặc đăng nhập bằng</span>
-                            </div> */}
+
                         </div>
-                        {/* <div className="mt-4 sm:mt-6 flex justify-center">
-                            <button
-                                type="button"
-                                onClick={handleGoogleRedirect}
-                                className="flex items-center justify-center px-4 sm:px-6 py-2 border border-gray-300 rounded-md shadow-sm bg-white hover:bg-gray-50"
-                            >
-                                <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" className="h-4 w-4 sm:h-5 sm:w-5">
-                                        <path fill="#4285F4" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-                                        <path fill="#34A853" d="M168.9 350.2L212.7 470 340.9 136.1 168.9 350.2z" />
-                                        <path fill="#FBBC05" d="M168.9 350.2L212.7 470 340.9 136.1 168.9 350.2z" />
-                                        <path fill="#EA4335" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </div> */}
+
                     </div>
                     <div className="mt-4 sm:mt-6 text-center">
                         <a href="/forgot-password" className="text-green-600 hover:underline text-xs sm:text-sm">
